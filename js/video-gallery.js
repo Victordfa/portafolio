c18dfc63e8139a1a80fc5839a61b7a6bcf24b0d8
Victordfa <Victordfa@users.noreply.github.com>
2019-12-04 21:18:42 -0600

    commit page 2d

---
+++ js/video-gallery.js
@@ -0,0 +1,22 @@
+<script
+  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
+<script>js/jquery.fancybox.min.js</script>
+
+<script>
+  $(document).ready(function() {
+    $('.fancybox').fancybox({
+      padding   	: 0,
+      maxWidth  	: '100%',
+      maxHeight 	: '100%',
+      width   		: 560,
+      height    	: 315,
+      autoSize  	: true,
+      closeClick  : true,
+      openEffect  : 'elastic',
+      closeEffect : 'elastic'
+    });
+  });
+</script>
